953ed5397bf3919eca2c2e3b15214549364ce085
Eugene Lee <elee05@mica.edu>
2021-03-24 20:50:03 +0900

    Step 1

---
+++ Project_backup/src/Pack_backup/Hello.java
@@ -0,0 +1,10 @@
+package Pack_backup;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// https://github.com/itseugene/Java_Practice01
+	}
+
+}
